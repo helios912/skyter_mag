1e6116bd2437ac0022c51a2b1d45836b4fb9a900
giga <das19901990a@gmail.com>
2024-12-01 08:26:44 +0200

    color

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
+    $('.baner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="baner-section__slider-btn baner-section__slider-btnprev"><img src="/images/arrow-right.svg"></button>',
+        nextArrow: '<button class="baner-section__slider-btn baner-section__slider-btnnext"><img src="/images/arrow-left.svg"></button>',
+    });
+    
 
-});
+}); 
