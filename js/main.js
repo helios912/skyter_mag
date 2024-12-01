893411b4ff34c0633032c71e902cc2bd431bc3d9
Mikola <das19901990a@gmail.com>
2024-12-01 19:03:08 +0200

    tabs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-$(function(){
+$(function () {
     $('.baner-section__slider').slick({
         dots: true,
-        prevArrow: '<button class="baner-section__slider-btn baner-section__slider-btnprev"><img src="/images/arrow-right.svg"></button>',
-        nextArrow: '<button class="baner-section__slider-btn baner-section__slider-btnnext"><img src="/images/arrow-left.svg"></button>',
+        prevArrow:
+            '<button class="baner-section__slider-btn baner-section__slider-btnprev"><img src="/images/arrow-right.svg"></button>',
+        nextArrow:
+            '<button class="baner-section__slider-btn baner-section__slider-btnnext"><img src="/images/arrow-left.svg"></button>',
     });
-    
-
-}); 
+    $('.search__tabs-item').on('click', function (e) {
+        e.preventDefault();
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+        $(this).addClass('search__tabs-item--active');
+        $($(this).attr('href')).addClass('search__content-item--active');
+    });
+});
