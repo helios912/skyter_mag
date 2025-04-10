72d82c1b4cecfa9d756f933cd110f65f03ed46ff
Mikola <das19901990a@gmail.com>
2025-04-10 12:28:15 +0300

    product

--- js/main.js
+++ js/main.js
@@ -16,9 +16,44 @@ $(function () {
         $($(this).attr('href')).addClass('tabs-content--active');
     });
 });
-$('.product-item__favorite').on('click',function(){
-
-
-
-  $('.product-item__favorite').toggleClass('product-item__favorite--active')
-})
+$('.product-item__favorite').on('click', function () {
+    $(this).toggleClass('product-item__favorite--active');
+});
+$('.product-slider').slick({
+    infinite: false,
+    speed: 300,
+    prevArrow:
+        '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="/images/arrow-black-right.svg"></button>',
+    nextArrow:
+        '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="/images/arrow-black-left.svg"></button>',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            },
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ],
+});
